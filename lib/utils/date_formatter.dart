7e183fe0e208eda65f61cd16d042dcb35106af59
jasimrazi <jasimrazi17@gmail.com>
2024-08-25 20:34:05 +0530

    Add calender toggle

---
+++ lib/utils/date_formatter.dart
@@ -0,0 +1,6 @@
+import 'package:intl/intl.dart';
+
+String formatDate(DateTime date) {
+  final DateFormat formatter = DateFormat('MMM d');
+  return formatter.format(date);
+}
--- lib/views/habitlist_page.dart
+++ lib/views/habitlist_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/models/habit_model.dart';
+import 'package:habit_tracker/utils/date_formatter.dart';
 import 'package:habit_tracker/viewmodels/auth_viewmodel.dart';
 import 'package:provider/provider.dart';
 import 'package:habit_tracker/viewmodels/habit_viewmodel.dart';
@@ -38,6 +39,8 @@ class _HabitListPageState extends State<HabitListPage>
   Widget build(BuildContext context) {
     final habitViewModel = Provider.of<HabitViewModel>(context);
     final habits = habitViewModel.getHabitsForDate(_selectedDate);
+
+    final String formattedDate = formatDate(_selectedDate);
 
     return Scaffold(
       appBar: AppBar(
@@ -97,92 +100,99 @@ class _HabitListPageState extends State<HabitListPage>
                   : SizedBox.shrink(),
             ),
             SizedBox(height: 30),
-            Text('Your Habits for the day'),
+            Text('Your Habits for $formattedDate'),
             Expanded(
-              child: ListView.builder(
-                itemCount: habits.length,
-                itemBuilder: (context, index) {
-                  final habit = habits[index];
-                  return ListTile(
-                    title: Text(habit.name),
-                    trailing: Checkbox(
-                      value: habit.isCompleted,
-                      onChanged: (bool? value) {
-                        // Toggle completion logic
+              child: habits.isEmpty
+                  ? Center(
+                      child: Text(
+                        'No habits',
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: habits.length,
+                      itemBuilder: (context, index) {
+                        final habit = habits[index];
+                        return ListTile(
+                          title: Text(habit.name),
+                          trailing: Checkbox(
+                            value: habit.isCompleted,
+                            onChanged: (bool? value) {
+                              // Toggle completion logic
+                              Provider.of<HabitViewModel>(context,
+                                      listen: false)
+                                  .toggleCompletion(habit.id);
+                            },
+                          ),
+                          onTap: () => _showEditDialog(context, habit),
+                        );
                       },
                     ),
-                  );
-                },
-              ),
             ),
+
           ],
         ),
       ),
-       floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         onPressed: () => _showAddDialog(context),
         child: Icon(Icons.add),
       ),
     );
   }
 
-  
-
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
-  
 }
 
-
 void _showAddDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Add Habit'),
-          content: TextField(
-            autofocus: true,
-            decoration: InputDecoration(hintText: 'Enter habit name'),
-            onSubmitted: (name) {
-              Provider.of<HabitViewModel>(context, listen: false)
-                  .addHabit(name);
-              Navigator.of(context).pop();
-            },
-          ),
-        );
-      },
-    );
-  }
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text('Add Habit'),
+        content: TextField(
+          autofocus: true,
+          decoration: InputDecoration(hintText: 'Enter habit name'),
+          onSubmitted: (name) {
+            Provider.of<HabitViewModel>(context, listen: false).addHabit(name);
+            Navigator.of(context).pop();
+          },
+        ),
+      );
+    },
+  );
+}
 
-  void _showEditDialog(BuildContext context, Habit habit) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Edit Habit'),
-          content: TextField(
-            autofocus: true,
-            controller: TextEditingController(text: habit.name),
-            decoration: InputDecoration(hintText: 'Enter new habit name'),
-            onSubmitted: (name) {
+void _showEditDialog(BuildContext context, Habit habit) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text('Edit Habit'),
+        content: TextField(
+          autofocus: true,
+          controller: TextEditingController(text: habit.name),
+          decoration: InputDecoration(hintText: 'Enter new habit name'),
+          onSubmitted: (name) {
+            Provider.of<HabitViewModel>(context, listen: false)
+                .editHabit(habit.id, name);
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
               Provider.of<HabitViewModel>(context, listen: false)
-                  .editHabit(habit.id, name);
+                  .deleteHabit(habit.id);
               Navigator.of(context).pop();
             },
+            child: Text('Delete'),
           ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Provider.of<HabitViewModel>(context, listen: false)
-                    .deleteHabit(habit.id);
-                Navigator.of(context).pop();
-              },
-              child: Text('Delete'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+        ],
+      );
+    },
+  );
+}
